bea93ff68a312a7d87fcaadf7f639d53e647ed57
MAK CHUN FUNG <bee.1845698825@gmail.com>
2023-12-29 23:06:25 +0800

    fix router base name

--- src/routers/Routers.tsx
+++ src/routers/Routers.tsx
@@ -3,19 +3,24 @@ import { RouterPath } from "@/constants/router";
 import { MainLayout } from "@/layouts";
 import { Planner, Backup } from "@/pages";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: RouterPath.ROOT,
+      element: <MainLayout />,
+      children: [
+        {
+          path: RouterPath.PLANNER,
+          element: <Planner />,
+        },
+        {
+          path: RouterPath.BACKUP,
+          element: <Backup />,
+        },
+      ],
+    },
+  ],
   {
-    path: RouterPath.ROOT,
-    element: <MainLayout />,
-    children: [
-      {
-        path: RouterPath.PLANNER,
-        element: <Planner />,
-      },
-      {
-        path: RouterPath.BACKUP,
-        element: <Backup />,
-      },
-    ],
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
